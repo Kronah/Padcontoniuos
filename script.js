b9479bf69a0966acea510afd45aa65143374628a
Kronah <kronah.contas@gmail.com>
2025-06-04 20:11:00 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,25 +1,28 @@
-let currentPad = null;
-let currentNote = "";
+let pads = {};
 
-function togglePad(note) {
-  if (currentNote === note && currentPad) {
-    stopPad();
-    return;
+function togglePad(id, button) {
+  if (pads[id]) {
+    pads[id].pause();
+    pads[id].currentTime = 0;
+    delete pads[id];
+    button.innerText = "OFF";
+    button.style.backgroundColor = "red";
+  } else {
+    const audio = new Audio("pads/" + id + ".mp3");
+    audio.loop = true;
+    audio.play();
+    pads[id] = audio;
+    button.innerText = "ON";
+    button.style.backgroundColor = "green";
   }
-
-  stopPad(); // Para o anterior
-
-  currentPad = new Audio(`pads/pad_${note}.mp3`);
-  currentPad.loop = true;
-  currentPad.play();
-  currentNote = note;
 }
 
-function stopPad() {
-  if (currentPad) {
-    currentPad.pause();
-    currentPad.currentTime = 0;
-    currentPad = null;
-    currentNote = "";
-  }
+function adjustVolume(elem, event) {
+  const rect = elem.getBoundingClientRect();
+  const height = rect.height;
+  const offset = event.clientY - rect.top;
+  const nivel = elem.querySelector(".nivel");
+  const percent = Math.max(0, Math.min(1, (height - offset) / height));
+  nivel.style.height = (percent * 100) + "%";
+  // Ajustar volume real se necessário
 }
